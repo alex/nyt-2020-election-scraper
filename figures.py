80734a08f72be41547c2396c9798c73e47e834a8
James Taylor <jimtaylor011@gmail.com>
2020-11-05 17:08:29 -0600

    Added more data to figures

--- figures.py
+++ figures.py
@@ -8,6 +8,48 @@ def update_figures():
 
     for i, df in enumerate(dfs):
         df = df[::-1]
-        fig = px.line(df, x="Timestamp", y="Vote Differential", title=order[i])
+        split = df["Block Breakdown"].fillna("Biden 0% / Trump 0%")
+        Biden = []
+        Trump = []
+        for x in split:
+            items = x.split("/")
+            for item in items:
+                if "Biden" in item:
+                    try:
+                        Biden.append(float(item.strip(" ").split(" ")[1].strip("%")))
+                    except:
+                        Biden.append(float(item.strip(" ").split(" ")[0].strip("%")))
+                else:
+                    Trump.append(float(item.strip(" ").split(" ")[1].strip("%")))
+        change = [x for x in df["Change"]]
+        bvotes = []
+        tvotes = []
+        for c, b, t in zip(change, Biden, Trump):
+            bvotes.append(c * (b / 100))
+            tvotes.append(c * (t / 100))
+        df["Biden"] = bvotes
+        df["Trump"] = tvotes
+        df["Btotal"] = df["Biden"].cumsum()
+        df["Ttotal"] = df["Trump"].cumsum()
+        fig = px.line(df, x="Timestamp", y="Vote Differential")
+        fig.update_traces(line_color="#ffa600", name="Vote Differential")
+        fig.add_bar(x=df["Timestamp"], y=df["Biden"], name="Biden votes")
+        fig.add_bar(x=df["Timestamp"], y=df["Trump"], name="Trump votes")
+        fig.add_scatter(
+            x=df["Timestamp"],
+            y=df["Btotal"],
+            mode="lines",
+            name="Biden",
+            line_color="#4634eb",
+        )
+        fig.add_scatter(
+            x=df["Timestamp"],
+            y=df["Ttotal"],
+            mode="lines",
+            name="Trump",
+            line_color="#eb4034",
+        )
         fig.write_html(f"figures/{order[i]}.html")
 
+
+update_figures()
