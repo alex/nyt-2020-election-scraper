3cbab8615b2bcf07bfff7fa7a3085334c4f64b4b
Saleem Rashid <dev@saleemrashid.com>
2020-11-07 02:47:01 +0000

    Add Firefox screenshots

--- .github/screenshot/index.js
+++ .github/screenshot/index.js
@@ -1,5 +1,8 @@
+const finalhandler = require("finalhandler");
+const http = require("http");
 const path = require("path");
 const puppeteer = require("puppeteer");
+const serveStatic = require("serve-static");
 const url = require("url");
 
 const DEVICES = {
@@ -10,17 +13,53 @@ const DEVICES = {
 };
 
 const REPOSITORY_ROOT = path.resolve(__dirname, "../..");
-const HTML_URL = url.pathToFileURL(path.join(REPOSITORY_ROOT, "battleground-state-changes.html"));
+
+/* Exit on uncaught asynchronous exceptions to propagate the error to CI */
+process.on("unhandledRejection", (reason, promise) => {
+    console.error(reason);
+    process.exit(1);
+});
 
 (async function () {
+    /*
+     * Firefox's Puppeteer implementation doesn't play well with file:// URLs,
+     * see https://github.com/puppeteer/puppeteer/issues/5504.
+     *
+     * As a workaround, run an HTTP server. :(
+     */
+    const serve = serveStatic(REPOSITORY_ROOT);
+    const server = http.createServer((request, response) => {
+        /* https://expressjs.com/en/resources/middleware/serve-static.html#serve-files-with-vanilla-nodejs-http-server */
+        serve(request, response, finalhandler(request, response));
+    });
+    server.listen();
+
+    const address = server.address();
+    const pageUrl = url.format({
+        protocol: "http",
+        hostname: address.address,
+        port: address.port,
+        pathname: "battleground-state-changes.html",
+    });
+    console.log(`Server listening at ${pageUrl}`);
+
+    /*
+     * Puppeteer refuses to install Chromium and Firefox at the same time, so
+     * we have to use PUPPETEER_PRODUCT to choose which browser to install and
+     * use. Sigh.
+     */
+    const product = puppeteer.product;
+    console.log(`Launching ${product}`);
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     async function screenshot(filename) {
-        await page.goto(HTML_URL, { waitUntil: "networkidle0" });
+        console.log(`Generating ${filename}`);
+        await page.goto(pageUrl, { waitUntil: "load" });
 
         await page.screenshot({
-            path: path.join(REPOSITORY_ROOT, `screenshot-${filename}.png`),
+            path: path.join(REPOSITORY_ROOT, `screenshot-${product}-${filename}.png`),
             fullPage: true,
         });
 
@@ -33,13 +72,13 @@ const HTML_URL = url.pathToFileURL(path.join(REPOSITORY_ROOT, "battleground-stat
             })();
         });
         await page.screenshot({
-            path: path.join(REPOSITORY_ROOT, `screenshot-${filename}-scrolled.png`),
+            path: path.join(REPOSITORY_ROOT, `screenshot-${product}-${filename}-scrolled.png`),
         });
     }
 
     await page.setViewport({
-        width: 1920,
-        height: 1080,
+        width: 1280,
+        height: 720,
     });
     await screenshot("desktop");
 
@@ -49,4 +88,5 @@ const HTML_URL = url.pathToFileURL(path.join(REPOSITORY_ROOT, "battleground-stat
     }
 
     await browser.close();
+    server.close();
 })();
